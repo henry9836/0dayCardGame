2f23b0ce43044d6679ee71435e44321f5398dec1
henry9836 <henry983615@gmail.com>
2019-08-12 04:30:09 +1200

    Ready to start

---
+++ 0day/0day/main.cpp
@@ -0,0 +1,20 @@
+#include <glew.h>
+#include <freeglut.h>
+#include <vector>
+#include <SOIL.h>
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+#include <ft2build.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+
+
+	return 0;
+
+}
