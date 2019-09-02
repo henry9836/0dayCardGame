30c54eda8c816b98e41046b83afef4355bc804fb
John Strange <john.Strange.nz@gmail.com>
2019-09-02 15:18:00 +1200

    Menu Merged

--- 0day/0day/Character.h
+++ 0day/0day/Character.h
@@ -12,7 +12,7 @@ public:
 	virtual void constantuUpdateLines(float deltaTime);
 	virtual void UpdateLines(float Diffrence);
 	virtual bool checkIfPlayable(float cost);
-	virtual float getHpBarPersent() { currentHP / maxHP; };
+	virtual float getHpBarPercent() { return currentHP / maxHP;};
 	//deck
 
 protected:
