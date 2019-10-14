cc23910f8b2be6c6de5e5742652091adf56a3d7f
Henry <henry983615@gmail.com>
2019-10-14 15:38:09 +1300

    moved text

--- 0day/0day/GameManager.cpp
+++ 0day/0day/GameManager.cpp
@@ -565,7 +565,7 @@ void populateGameObjectList() {
 
 	game->lostObjects.push_back(new GameObject(new RenderText(new CTextLabel("Game Over\nYou have lost the battle againest the robots\nPress Space To Continue...", Utility::NormalFontString.data(), glm::vec2(game->ScreenSize.x * -10.0f, game->ScreenSize.y * 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), 1.0f, game, ("Lose Text"))), new IdleTick, Transform(glm::vec3(game->ScreenSize.x * -0.35f, 0.0f, 0.0f), glm::vec3(0, 0, 0), glm::vec3(0, 0, 0)), "Lose Text"));
 
-	game->levelText = new CTextLabel("Level: 1", Utility::NormalFontString.data(), glm::vec2(0.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), 1.0f, game, ("Level Text"));
+	game->levelText = new CTextLabel("Level: 1", Utility::NormalFontString.data(), glm::vec2(game->ScreenSize.x * -0.45f, game->ScreenSize.y * 0.45f), glm::vec3(1.0f, 1.0f, 1.0f), 1.0f, game, ("Level Text"));
 #pragma endregion
 
 
